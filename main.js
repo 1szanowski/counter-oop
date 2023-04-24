7a960c338e67b12a3d617f1b46d32348153cae9a
Aleks Szanowski <117806276+1szanowski@users.noreply.github.com>
2023-04-25 00:30:32 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <h1>People entered:</h1>
+        <h2 id="count">0</h2>
+        <button id = "incrementer">increment</button>
+        <button id = "decrementer">Decrement</button>
+        <button id = "reset">Reset</button>
+    </body>
+    <script src="index.js"></script>
+</html>
